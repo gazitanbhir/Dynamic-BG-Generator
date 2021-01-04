d51b6fa6444472670131122ac1bbb645404fccd7
Your Name <gazitanbhir@gmail.com>
2021-01-04 15:42:41 +0600

    Added all files to github

---
+++ home.js
@@ -0,0 +1,12 @@
+var css = document.querySelector("h3");
+var c1 = document.querySelector("#c1");
+var c2 = document.querySelector("#c2");
+var body = document.getElementById("gr");
+
+function setGr()
+{
+	body.style.background = "linear-gradient(to right, " + c1.value + ", " + c2.value + ")";
+}
+
+c1.addEventListener("input", setGr );
+c2.addEventListener("input", setGr );
